68a1547211c1aa9c293c98c524e8e100329aad85
timothyadetunji <adetunji@yahoo.com>
2014-10-02 20:52:05 +0100

    this is a cpu count script

--- script.sh
+++ script.sh
@@ -3,5 +3,12 @@
 #: Date : 02 10 2014
 #: Author : 
 #: Version :
-#: Description : 
+#: Description : This is a cpu count script
 #: Options : 
+
+cpu_count=`cat /proc/cpuinfo |grep processor | wc -l`
+
+echo "There are $cpu_count cpu in this machine"
+
+
+
